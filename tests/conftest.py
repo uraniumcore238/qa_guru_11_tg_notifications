6b2ec85ecfe7e24496f0df139a05f769a4f18218
uraniumcore238 <uraniumcore238@gmail.com>
2022-07-30 18:48:49 +0300

    first commit

---
+++ tests/conftest.py
@@ -0,0 +1,51 @@
+import os
+
+import pytest
+from selenium.webdriver.chrome.options import Options
+from selenium import webdriver
+from selene.support.shared import browser
+from selene import Browser, Config
+from dotenv import load_dotenv
+from utils import attach
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--browser_version',
+        default='100.0'
+    )
+
+
+@pytest.fixture(scope='session', autouse=True)
+def load_env():
+    load_dotenv()
+
+
+@pytest.fixture(scope='function')
+def setup_browser(request):
+    browser_version = request.config.getoption('--browser_version')
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": 'chrome',
+        "browserVersion": browser_version,
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": True
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    login = os.getenv('LOGIN')
+    password = os.getenv('PASSWORD')
+    driver = webdriver.Remote(command_executor=f"https://{login}:{password}@selenoid.autotests.cloud/wd/hub", options=options)
+    # driver = webdriver.Remote(command_executor="http://localhost:4444/wd/hub", options=options)
+
+    browser.config.driver = driver
+
+    yield browser
+
+    attach.add_html(browser)
+    attach.add_screenshot(browser)
+    attach.add_logs(browser)
+    attach.add_video(browser)
+    browser.quit()
+
